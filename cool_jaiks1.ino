0efc973e8a4aaeb183d0e2e3d0c1eb8c17ffe685
Fernando S. Axis <159575977+fernandoaxis@users.noreply.github.com>
2025-03-27 22:31:05 -0300

    Update cool_jaiks1.ino

--- cool_jaiks1.ino
+++ cool_jaiks1.ino
@@ -3,10 +3,11 @@ int ledAmareloC = 2;
 int ledVerdeC = 1;
 int ledVermelhoP = 7;
 int ledVerdeP = 8;
-int botao = 10;
+bool botao = false;
+int botao1 = 10;
 
 void setup() {
-  pinMode(botao, INPUT_PULLUP);
+  pinMode(botao1, INPUT_PULLUP);
   pinMode(ledVermelhoC, OUTPUT);
   pinMode(ledAmareloC, OUTPUT);
   pinMode(ledVerdeC, OUTPUT);
@@ -15,8 +16,17 @@ void setup() {
 }
 
 void loop() {
+  	
+  if((botao == false) && (digitalRead(botao1) == LOW)){
+    botao = true;
+   
+ 
+  }else if((botao == true) && (digitalRead(botao1) == LOW)){
+  	botao = false;
+  }
   
-    digitalWrite(ledVermelhoP, HIGH);
+  if(botao == true){
+  	digitalWrite(ledVermelhoP, HIGH);
     digitalWrite(ledVerdeC, HIGH);
     delay(2000);
     
@@ -32,5 +42,12 @@ void loop() {
   
   	digitalWrite(ledVermelhoC, LOW);
     digitalWrite(ledVerdeP, LOW);
- 
-  } 
+    
+  }else{
+  	digitalWrite(ledVermelhoC, LOW);
+    digitalWrite(ledAmareloC, LOW);
+  	digitalWrite(ledVerdeC, LOW);
+  	digitalWrite(ledVermelhoP, LOW);
+  	digitalWrite(ledVerdeP, LOW);
+    }
+  }
